a66917c621d1d34288e7ae5bdb10301c70c8e7e5
Haofei Ying <xdhfying@gmail.com>
2016-06-23 00:08:06 -0700

    added signin API for app logins

--- views/signin.py
+++ views/signin.py
@@ -3,6 +3,8 @@ from flask.ext.login import login_user
 
 from mysite import app
 from mysite.models.user import User
+from mysite.api.utils import ok, error
+from mysite.api.const import Error
 
 
 @app.route('/signin/', methods=['GET', 'POST'])
@@ -32,3 +34,28 @@ def signin():
     login_user(registered_user, remember_me)
     flash('Login successfully')
     return redirect(url_for('board'))
+
+
+@app.route('/signin2/', methods=['POST'])
+def signin2():
+    """
+    Used as REST API.
+    """
+    netid = request.form['netid'].strip()
+    password = request.form['password'].strip()
+    remember_me = 'remember_me' in request.form
+
+    registered_user = User.get_user(netid)
+
+    if not registered_user:
+        return error(Error.NETID_NOT_EXIST)
+
+    if not registered_user.check_password(password):
+        return error(Error.INVALID_PASSWORD)
+
+    login_user(registered_user, remember_me)
+
+    ret = registered_user.to_dict()
+    ret.pop("password")
+
+    return ok(ret)
